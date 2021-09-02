b9e80460c368c9170a8c77332ab307561848f6db
migucms <chunsead888@gmail.com>
2021-09-03 06:51:01 +0800

    [新增]新增主题模板显示目录 Signed-off-by: migucms <chunsead888@gmail.com>

--- application/common/behavior/Base.php
+++ application/common/behavior/Base.php
@@ -29,7 +29,7 @@ class Base
 {
     public function run()
     {
-        define('MIGU_VERSION', '2.0.29');
+        define('MIGU_VERSION', '2.0.31');
         define('ROOT_PATH', Env::get('root_path'));
         define('IN_SYSTEM', true);
         define('ROOT_DIR', '/');
--- version.php
+++ version.php
@@ -5,7 +5,7 @@ return [
   [
     'name' => '咪咕CMS Pro',
     'release' => 0,
-    'version' => '2.0.29',
+    'version' => '2.0.31',
     'copyright' => 'migucms.com',
     'url' => 'http://www.migucms.com',
   ],
